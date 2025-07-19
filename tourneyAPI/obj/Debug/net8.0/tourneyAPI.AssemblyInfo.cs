bb8df93ced943dd0b8abe44583daf9eb33bc47cf
nicholas hutter <nicholas.hutter@live.com>
2025-07-19 16:27:44 -0500

    fix strange duplicate build issue, rebase local branch

--- tourneyAPI/obj/Debug/net8.0/tourneyAPI.AssemblyInfo.cs
+++ tourneyAPI/obj/Debug/net8.0/tourneyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tourneyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e09fef6928270ef752858e6c6024c8f577d39c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895760a23d785006e2a7019c69645c3e174f5afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("tourneyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tourneyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
