650ab20875ada63b80b17f3df1fd4bc01644c699
nicholas hutter <nicholas.hutter@live.com>
2025-08-27 16:11:34 -0500

    front end unit testing introduced

--- tourneyClient/tests/RequestServiceTest.ts
+++ tourneyClient/tests/RequestServiceTest.ts
@@ -1,4 +1,7 @@
-import { test } from 'vitest';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { test, expect, vi, beforeEach } from 'vitest';
 import { RequestService } from '../src/utilities/RequestService';
 import { ApplicationUser } from '../src/models/entities/ApplicationUser';
 
@@ -13,14 +16,39 @@ const User: ApplicationUser =
     Email
 }
 
-test("RequestService submits a properly formatted http request", () =>
+beforeEach(
+    () => 
+    {
+        global.fetch = vi.fn();
+    }
+)
+
+//unit test level fetchService test mocking request formatting
+test("RequestService submits a properly formatted http request", async () =>
 {
-    RequestService("createUserSession",
+
+    (fetch as any).mockResolvedValueOnce(
+        {
+            ok: true,
+            json: (async: any) => (
+                {
+                    success: true
+                }
+            )
+        }
+    );
+
+    const result = await RequestService("createUserSession",
         {
             body:
             {
                 User
             }
-        })
+        });
+    expect(result).toEqual(
+        {
+            success: true
+        }
+    )
 
 })
