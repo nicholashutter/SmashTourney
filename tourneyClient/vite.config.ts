2be8945a3ab401927f68e24d6c0e116899b11c21
nicholas hutter <nicholas.hutter@live.com>
2025-08-28 13:46:26 -0500

    vitest now setup correctly test passes

--- tourneyClient/vite.config.ts
+++ tourneyClient/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +11,9 @@ export default defineConfig({
       react(),
       tailwindcss(),
     ],
+  test:
+  {
+    include: ['tests/**/*.test.ts'],
+    exclude: [...configDefaults.exclude],
+  },
 })
